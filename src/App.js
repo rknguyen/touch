762872bf0c3c968aa9906f7f183c73a91b58a07e
huyng12 <huy.nguyen@trustingsocial.com>
2021-01-22 00:35:47 +0700

    feat: intiate project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { H1 } from './components/Typo'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <H1>Nguyen Huy</H1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
