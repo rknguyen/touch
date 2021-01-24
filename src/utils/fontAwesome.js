74fab8847eeaa33f545866d2fe16502eecfbdbef
huyng12 <huy.nguyen@trustingsocial.com>
2021-01-24 11:55:59 +0700

    feat: fontawesome@v5

---
+++ src/utils/fontAwesome.js
@@ -0,0 +1,8 @@
+import { library as fontLibrary } from '@fortawesome/fontawesome-svg-core'
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin
+} from '@fortawesome/free-brands-svg-icons'
+
+fontLibrary.add(faFacebook, faInstagram, faLinkedin)
